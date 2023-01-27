52a35cf0933cf1ba028baf1fe27fe9ab886fae96
mumingiri <girimumin123@gmail.com>
2023-01-27 00:59:57 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// client/src/App.js
+
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
