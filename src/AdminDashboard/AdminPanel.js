ab8af7e4f3a629b4bd2422113fc67f008a78ffb9
mumingiri <girimumin123@gmail.com>
2023-02-10 19:53:45 -0800

    admin package

--- src/AdminDashboard/AdminPanel.js
+++ src/AdminDashboard/AdminPanel.js
@@ -46,6 +46,43 @@ const AdminPanel = () => {
         dataField.innerHTML = " <p>Error while fetching data</p>";
       }
     }
+    else if(dataToShow==="package"){
+      const dataField = document.querySelector(".div3")
+      try {
+        const response = await axios.post('http://localhost:5000/api/staff');
+        const data = response.data;
+        var result = `
+        <table>
+          <thead>
+            <tr>
+              <th>UserId</th>
+              <th>UserName</th>
+              <th>UserEmail</th>
+              <th>Address</th>
+              <th>Phone</th>
+              <th>UserDOB</th>
+            </tr>
+          </thead>
+        
+        <tbody>`
+        data.map((data) => (
+          result = result + `
+          <tr>
+          <td>${data.UserId}</td>
+          <td>${data.UserName}</td>
+          <td>${data.UserEmail}</td>
+          <td>${data.UserAddress}</td>
+          <td>${data.UserPhone}</td>
+          <td>${data.UserDOB.split("T")[0]}</td>
+        </tr>
+          `
+        ))
+      dataField.innerHTML = result;
+      } catch (e) {
+        
+        dataField.innerHTML = " <p>Error while fetching data</p>";
+      }
+    }
   }
   return (
     <>
@@ -69,3 +106,28 @@ const AdminPanel = () => {
 }
 
 export default AdminPanel
+
+{/* <table>
+<thead>
+  <tr>
+    <th>UserId</th>
+    <th>UserName</th>
+    <th>UserEmail</th>
+    <th>Address</th>
+    <th>Phone</th>
+    <th>UserDOB</th>
+  </tr>
+</thead>
+
+<tbody>
+  <tr>
+    <td>cell1_1</td>
+    <td>cell2_1</td>
+    <td>cell3_1</td>
+    <td>cell4_1</td>
+    <td>cell5_1</td>
+    <td>cell6_1</td>
+  </tr>
+  
+</tbody>
+</table> */}
